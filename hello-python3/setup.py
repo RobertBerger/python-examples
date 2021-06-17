669aacab1ccca4c3c3e5f226a0fa17eadcbeece5
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-06-17 17:17:08 +0300

    initial commit

---
+++ hello-python3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Python package setup file."""
+
+from setuptools import setup
+
+setup(
+    name='hello-python3',
+    version='1.0.0',
+    scripts=['hello-python3.py'],
+    license_files=('LICENSE',),
+)
